6094a213828b69f045266398f1ef253fdefe5ed0
KyleAxley <kanderson7645@yahoo.com>
2022-09-08 15:03:16 -0400

    user pizza routes created

---
+++ server.js
@@ -0,0 +1,23 @@
+//import for mongoose package 
+const mongoose = require ('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+//connection to mongoose route
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
